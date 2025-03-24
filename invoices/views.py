93091a1788e7428dd0229bc89187086538a516e0
Victor Braz <victor.90braz@gmail.com>
2025-03-24 11:33:46 +0100

    add pagination

--- invoices/views.py
+++ invoices/views.py
@@ -1,10 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
 from .models import Invoice
 from .serializers import InvoiceSerializer
 
 @api_view(['GET'])
-def invoice_list(request):
+def invoice_list(request: Request) -> Response:
     invoices = Invoice.objects.all()
-    serializer = InvoiceSerializer(invoices, many=True)
-    return Response(serializer.data)
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    result_page = paginator.paginate_queryset(invoices, request)
+
+    serializer = InvoiceSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
