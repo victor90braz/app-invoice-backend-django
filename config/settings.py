e7f33639132a5b0daa67e1db1e29f06cfdff7bf7
Victor Braz <victor.90braz@gmail.com>
2025-03-24 11:58:38 +0100

    add pagination

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Extra opcional para desarrollo local
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
